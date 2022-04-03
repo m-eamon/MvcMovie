7d7c26fb6c800c5715f43e07dc2a4e80c197ebaf
m-eamon <eamon.muldowney@gmail.com>
2022-04-03 17:00:38 +0100

    Fix controller for Create & Edit

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -54,7 +54,7 @@ namespace MvcMovie.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public async Task<IActionResult> Create([Bind("Id,Title,Category,Description,Price,Retailer")] Movie movie)
         {
             if (ModelState.IsValid)
             {
@@ -86,7 +86,7 @@ namespace MvcMovie.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Category,Description,Price,Retailer")] Movie movie)
         {
             if (id != movie.Id)
             {
